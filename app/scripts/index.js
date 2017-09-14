643b0c7b31e39dcb48165224af96338d029aebee
Ary Davani <arydavani@gmail.com>
2017-09-14 12:13:02 -0400

    added loading template

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,31 +1,56 @@
 var $ = require('jquery');
 var Redux = require('redux');
 var indexTemplate = require('../templates/index.hbs');
+var loadingTemplate = require('../templates/load.hbs');
+
+var RUN_SEARCH = 'RUN_SEARCH';
 
 var INITIAL_STATE = {
+  showLoading: false,
   name: '',
-  hipsterLevel: 0
+  hipsterLevel: 0,
 };
 
 
-function puppyReducer( state = INITIAL_STATE, action ) {
-  console.log(action);
-  // switch (action.type) {
-  //   case 'TO_GREEN':
-  //     return state.backgroundColor = 'green';
-  //   default:
-  //     return state;
-  return state;
+function puppyReducer(state, action) {
+  switch (action.type) {
+    case RUN_SEARCH:
+      state.name = action.name;
+      state.hipsterLevel = action.hipsterLevel;
+      state.showLoading = true;
+
+    default:
+      return state;
+  };
 };
 
 var store = Redux.createStore(puppyReducer, INITIAL_STATE);
+store.subscribe(showLoading);
 
-$('#app').html(indexTemplate());
+function showHome() {
+  $('#app').html(indexTemplate());
 
-$('#puppy-search').on('submit', function(e) {
-  e.preventDefault();
-  var name = $('#name').val();
-  var hipsterLevel = $('#hipster-level').val();
+  $('#puppy-search').on('submit', function(e) {
+    e.preventDefault();
 
-  store.dispatch({type: 'RUN_SEARCH', name: name, hispterLevel: hipsterLevel});
-})
+    var name = $('#name').val();
+    var hipsterLevel = $('#hipster-level').val();
+
+    store.dispatch({type: RUN_SEARCH, name: name, hipsterLevel: hipsterLevel});
+
+  });
+};
+
+function showLoading() {
+  console.dir(store.getState());
+  var state = store.getState();
+
+  // if we're not supposed to show the loading screen, bail
+  if (!state.showLoading) {
+    return;
+  }
+
+  $('#app').html(loadingTemplate());
+};
+
+showHome();
